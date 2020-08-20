f8d329a39f689b9543ead1f3152921a35fd600b9
Mateus Santos <mateusosantos@outlook.com.br>
2020-08-19 23:37:08 -0300

    Added first migration

--- src/main/java/com/mateus/os/domain/model/Client.java
+++ src/main/java/com/mateus/os/domain/model/Client.java
@@ -1,7 +1,19 @@
 package com.mateus.os.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
 	private String name;
 	private String email;
 	private String phone;
